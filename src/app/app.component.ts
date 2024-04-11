70731f060816a49d16dea56ebfea6faf87d92883
Patricia <patriciastefanut@yahoo.com>
2024-04-11 19:03:46 +0300

    first commit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'employeeApp';
+
+  stepsList: any []= [
+    { stepName: 'Basic Details', isComplete: false},
+    { stepName: 'Skills', isComplete: false},
+    { stepName: 'Experience', isComplete: false},
+  ]
+
+  activeStep: any = this.stepsList[1];
+
+  setActiveStep(activeStep: any) {
+    this.activeStep= activeStep;
+  }
+
 }
