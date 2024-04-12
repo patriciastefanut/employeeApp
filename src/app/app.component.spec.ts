138c487d54dc28bd35f813e504275471d17d76c0
Patricia <patriciastefanut@yahoo.com>
2024-04-12 11:00:42 +0300

    final version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'employeeApp' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('employeeApp');
-  });
+  // it(`should have the 'Employee_stepper_app_angular_17' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('Employee_stepper_app_angular_17');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, employeeApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Employee_stepper_app_angular_17');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,FormsModule,HttpClientModule],
+  imports: [CommonModule,FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  designationList: any [] = []; 
-  roleList: any [] = []; 
+export class AppComponent  implements OnInit{
+  designationList: any [] = [];
+  roleList: any [] = [];
+  stepsList: any [] = [
+    { stepName: 'Basic Details',isComplete: false},
+    { stepName: 'Skills',isComplete: false},
+    { stepName: 'Experience',isComplete: false}
+  ];
+  activeStep:  any =  this.stepsList[0];
 
-  stepsList: any []= [
-    { stepName: 'Basic Details', isComplete: false},
-    { stepName: 'Skills', isComplete: false},
-    { stepName: 'Experience', isComplete: false},
-  ]
-
-  activeStep: any = this.stepsList[0];
-
+  stepperCompletionValue:number = 8;
   employeeObj: any = {
     "roleId": 0,
-    "userName": " ",
-    "empCode": "",
     "empId": 0,
     "empName": "",
     "empEmailId": "",
@@ -44,29 +41,21 @@ export class AppComponent implements OnInit {
     "empPerPinCode": "",
     "empPerAddress": "",
     "password": "",
-    erpEmployeeSkills: [],
-    ermEmpExperiences: [],
+    erpEmployeeSkills:[],
+    ermEmpExperiences:[]
   }
   
-/*  empSkillObj: any = {
-      "empSkillId": 0,
-      "empId": 0,
-      "skill": "string",
-      "totalYearExp": 0,
-      "lastVersionUsed": "string"
-    }
-  
-*/
- 
-empExpObj: any = {
-      "empExpId": 0,
-      "empId": 0,
-      "companyName": "string",
-      "startDate": "2024-04-11T16:38:32.716Z",
-      "endDate": "2024-04-11T16:38:32.716Z",
-      "designation": "string",
-      "projectsWorkedOn": "string"
+  empExpObj : any  = {
+    "empExpId": 0,
+    "empId": 0,
+    "companyName": "string",
+    "startDate": "2024-03-05T16:58:07.710Z",
+    "endDate": "2024-03-05T16:58:07.710Z",
+    "designation": "string",
+    "projectsWorkedOn": "string"
   }
+  employeeList: any[]=[];
+  isCreateView: boolean= false;
 
   constructor(private http: HttpClient) {
 
@@ -77,6 +66,57 @@ empExpObj: any = {
     this.loadRoles();
   }
 
+  addNew() {
+    this.isCreateView = true;
+  }
+  setActiveStep(activeStep: any) {
+    this.activeStep = activeStep;
+  }
+  gotoStep2() {
+    const currentStep = this.stepsList.find(m=>m.stepName == this.activeStep.stepName);
+    currentStep.isComplete = true;
+    this.activeStep = this.stepsList[1];
+    this.stepperCompletionValue = 50;
+  }
+  gotoStep3() {
+    const currentStep = this.stepsList.find(m=>m.stepName == this.activeStep.stepName);
+    currentStep.isComplete = true;
+    this.activeStep = this.stepsList[2];
+    this.stepperCompletionValue = 100;
+  }
+
+  addSkills() {
+    const skillObj = {
+      "empSkillId": 0,
+      "empId": 0,
+      "skill": "",
+      "totalYearExp": 0,
+      "lastVersionUsed": ""
+    };
+
+    this.employeeObj.erpEmployeeSkills.unshift(skillObj)
+  }
+  addExp() {
+    const expObj = {
+      "empExpId": 0,
+      "empId": 0,
+      "companyName": " ",
+      "startDate": "",
+      "endDate": "",
+      "designation": "",
+      "projectsWorkedOn": ""
+    };
+
+    this.employeeObj.ermEmpExperiences.unshift(expObj)
+  }
+
+  onEdit(id: number){
+    this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetEmployeeByEmployeeId?id=" +id).subscribe((res:any)=>{
+      this.employeeObj = res.data;
+      this.employeeObj.empId =  id;
+      this.isCreateView = true;
+    })
+  }
   loadAllEmployees() {
     this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllEmployee").subscribe((res:any) => {
       this.employeeList = res.data;
@@ -93,42 +133,25 @@ empExpObj: any = {
       this.roleList = res.data;
     })
   }
-
-  setActiveStep(activeStep: any) {
-    this.activeStep= activeStep;
+ 
+  onDelet(id:number) {
+    const isDelete = confirm("Are You sure want to delete");
+    if(isDelete) {
+      this.http.delete("https://freeapi.gerasim.in/api/EmployeeApp/DeleteEmployeeByEmpId?empId="+id).subscribe((res:any)=>{
+        if(res.result) {
+          alert('Employee Deleted Success');
+          this.loadAllEmployees();
+        } else {
+          alert(res.message)
+        }
+      })
+    }
   }
-
-  addSkills() {
-    const skillObj = {
-      "empSkillId": 0,
-      "empId": 0,
-      "skill": "",
-      "totalYearExp": 0,
-      "lastVersionUsed": ""
-    };
-
-    this.employeeObj.erpEmployeeSkills.unshift(skillObj)
-  }
-
-  addExp() {
-    const expObj = {
-      "empExpId": 0,
-      "empId": 0,
-      "companyName": " ",
-      "startDate": "",
-      "endDate": "",
-      "designation": "",
-      "projectsWorkedOn": ""
-    };
-
-    this.employeeObj.ermEmpExperiences.unshift(expObj)
-  }
-
   saveEmployee() {
     debugger;
     this.http.post("https://freeapi.gerasim.in/api/EmployeeApp/CreateNewEmployee", this.employeeObj).subscribe((res:any)=>{
       if(res.result) {
-        alert('Employee Created Successfully');
+        alert('Employee Created Success');
         this.loadAllEmployees();
         this.isCreateView = false;
       } else {
@@ -137,4 +160,15 @@ empExpObj: any = {
     })
   }
 
+  updateEmployee() {
+    this.http.put("https://freeapi.gerasim.in/api/EmployeeApp/UpdateEmployee", this.employeeObj).subscribe((res:any)=>{
+      if(res.result) {
+        alert('Employee Created Success')
+        this.loadAllEmployees();
+        this.isCreateView = false;
+      } else {
+        alert(res.message)
+      }
+    })
+  }
 }
